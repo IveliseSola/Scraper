8fa03384f16926134bde1087ff74df6e6789ef87
Ivelise Sola <solaivelise@yahoo.com>
2018-04-23 13:22:57 -0400

    server.js require

---
+++ server.js
@@ -0,0 +1,15 @@
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
+
+//My heroku link: https://limitless-peak-92095.herokuapp.com
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var db = require("./models");
