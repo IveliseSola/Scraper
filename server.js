bee8b2ed0d378a21c56a66113c0e89bfa9092f39
Ivelise Sola <solaivelise@yahoo.com>
2018-04-24 20:30:17 -0400

    bootstrap

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 // var request = require("request");
 var axios = require("axios");
+var path = require("path");
 var db = require("./models");
 
 var PORT = 3000;
@@ -52,7 +53,8 @@ app.get("/scrape", function (req, res) {
 app.get("/articles", function(req, res) {
   db.Article.find({})
     .then(function(dbArticle) {
-      res.json(dbArticle);
+      // res.json(dbArticle);
+      res.sendFile(path.join(__dirname, "../public/index.html"))
     })
     .catch(function(err) {
       res.json(err);
@@ -79,17 +81,14 @@ app.post("/articles/:id", function(req, res) {
  
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
+   
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
+  
       res.json(err);
     });
 });
